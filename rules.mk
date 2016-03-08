3b85499d2d8b70cdd52f5e02b86a4af8cdb93b36
Eberhard Beilharz <eb1@sil.org>
2016-03-08 14:09:05 +0100

    Replace fieldworks packages with lfmerge-fdo package
    This package is needed for lfmerge.
    
    NOTE: this branch is not intended to be merged into develop or
    master and should always live as a separate branch used for
    LfMerge.
    
    Change-Id: Ib77d260b2a82f886790aaeb82a97dfea602b5562

--- rules.mk
+++ rules.mk
@@ -8,9 +8,9 @@ DEB_SRCDIR = $(CURDIR)/fw
 DEB_BUILD_OPTIONS = nocheck
 DEB_MAKE_EXTRA_ARGS = BUILD_TYPE=r BUILD_CONFIG=Release
 
-DEB_MAKE_BUILD_TARGET   = Fw-build-package
-DEB_MAKE_INSTALL_TARGET = install-package DESTDIR=$(DEB_DESTDIR)
+DEB_MAKE_BUILD_TARGET   = Fw-build-package-fdo
+DEB_MAKE_INSTALL_TARGET = install-package-fdo DESTDIR=$(DEB_DESTDIR)
 
-DEB_SHLIBDEPS_INCLUDE = /usr/lib/fieldworks
-DEB_DH_MAKESHLIBS_ARGS_ALL = -X/usr/lib/fieldworks/
+DEB_SHLIBDEPS_INCLUDE = /usr/lib/fieldworks:/usr/lib/lfmerge
+DEB_DH_MAKESHLIBS_ARGS_ALL = -X/usr/lib/fieldworks/ -X/usr/lib/lfmerge
 DEB_DH_STRIP_ARGS = -k
