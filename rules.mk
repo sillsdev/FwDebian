4f55b47154928afb29f4c62786b81c2a3da31010
Eberhard Beilharz <eb1@sil.org>
2017-07-14 18:29:10 +0200

    Trying to fix package build
    Don't exclude /usr/lib/fieldworks from makeshlibs since that
    directory contains icu libraries. If this doesn't work we can try
    to exclude /usr/lib/fieldworks from shlibdeps.
    
    Change-Id: I4670f32ee95b9a26e649aa1173c64a803b4a9030

--- rules.mk
+++ rules.mk
@@ -17,5 +17,5 @@ DEB_MAKE_CHECK_TARGET   =
 
 DEB_DH_STRIP_ARGS = -k
 DEB_SHLIBDEPS_INCLUDE = /usr/lib/fieldworks
-DEB_DH_MAKESHLIBS_ARGS_ALL = -X/usr/lib/fieldworks/
+#DEB_DH_MAKESHLIBS_ARGS_ALL = -X/usr/lib/fieldworks/
 DEB_DH_SHLIBDEPS_ARGS_ALL = -X/usr/lib/fieldworks/Firefox/
